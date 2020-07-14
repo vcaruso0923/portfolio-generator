62c5dcfcf3e7e520b40415ba87b7fe4dba9c4ad2
vcaruso <vinnycar0923@gmail.com>
2020-07-14 10:35:12 -0700

    Create html file based on input

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
